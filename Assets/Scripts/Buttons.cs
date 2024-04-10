a895dd4b1e0e3ff2e8d55ee273de38bbbb6e1825
NoahSpielman <NSpielman@Clarku.edu>
2024-04-10 04:02:24 -0400

    I added sounds to the main menu buttons and put some music in the main menu and the map menu and let you go from the map menu back to the main menu

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -7,10 +7,20 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioClip menuNoise; 
+    public AudioClip menuSong; 
+    
+    public void start()
+    {
+        audioSource.Play();
+    }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("TestScene"); //eventually change TestScene to an integer variable that represents the furthest completed level
+        audioSource.clip = menuNoise;
+        audioSource.Play();
+
+        Invoke("startchange", 0.7f); //eventually change TestScene to an integer variable that represents the furthest completed level
     }
 
     public void Quit()
@@ -20,19 +30,37 @@ public class Buttons : MonoBehaviour
 
     public void Options()
     {
+        audioSource.clip = menuNoise;
         audioSource.Play();
         
-        SceneManager.LoadScene("Options Menu"); 
+        Invoke("optionschange", 0.7f); 
     }
 
     public void WorldMap()
     {
+        audioSource.clip = menuNoise;
         audioSource.Play();
-        SceneManager.LoadScene("WorldMap"); 
+        //SceneManager.LoadScene("WorldMap");
+        Invoke("mapchange", 0.7f); 
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void mapchange()
+    {
+        SceneManager.LoadScene("WorldMap");
+    }
+
+    public void optionschange()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+    public void startchange()
+    {
+        SceneManager.LoadScene("TestScene");
     }
 }
